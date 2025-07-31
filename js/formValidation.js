c49048516b3d1879363eafbc27617ca670962042
Chris-Tech-01 <ChrisFoleyTech@hotmail.com>
2025-07-31 16:15:04 +0100

    Updated Portfolio

--- js/formValidation.js
+++ js/formValidation.js
@@ -9,30 +9,30 @@ export function validateContactForm(form) {
     let isValid = true;
     let errors = [];
 
-if (!/^[A-Za-z\s\-']{2,}$/.test(firstName)) {
-    isValid = false;
-    errors.push('First name must be at least 2 letters and contain only letters, spaces, hyphens, or apostrophes.');
-}
+    if (!/^[A-Za-z\s\-']{2,}$/.test(firstName)) {
+        isValid = false;
+        errors.push('First name must be at least 2 letters and contain only letters, spaces, hyphens, or apostrophes.');
+    }
 
-if (!/^[A-Za-z\s\-']{2,}$/.test(lastName)) {
-    isValid = false;
-    errors.push('Last name must be at least 2 letters and contain only letters, spaces, hyphens, or apostrophes.');
-}
+    if (!/^[A-Za-z\s\-']{2,}$/.test(lastName)) {
+        isValid = false;
+        errors.push('Last name must be at least 2 letters and contain only letters, spaces, hyphens, or apostrophes.');
+    }
 
-if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-    isValid = false;
-    errors.push('Please enter a valid email address.');
-}
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        isValid = false;
+        errors.push('Please enter a valid email address.');
+    }
 
-if (subject.length < 3) {
-    isValid = false;
-    errors.push('Subject must be at least 3 characters.');
-}
+    if (subject.length < 3) {
+        isValid = false;
+        errors.push('Subject must be at least 3 characters.');
+    }
 
-if (message.length < 1 || message.length > 1000) {
-    isValid = false;
-    errors.push('Message must be between 1 and 1000 characters.');
-}
+    if (message.length < 1 || message.length > 1000) {
+        isValid = false;
+        errors.push('Message must be between 1 and 1000 characters.');
+    }
 
-return { isValid, errors };
+    return { isValid, errors };
 }
